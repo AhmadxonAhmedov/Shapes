262a42c2d45d35443fd3ce254224625791876b4e
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-02-21 03:00:40 +0500

    Add Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Write("ShuLardan birini kiriting : U | T \n");
+ char input = Convert.ToChar(Console.ReadLine());
+
+
+
+  switch(input)
+  {
+    case 'U':
+    Edge(6);
+    break;
+
+    case 'T':
+    Triangle(5,6);
+    break;
+  }
+
+
+void Edge(int height)
+
+{
+  for (int i = 1; i <= height; i++)
+  {
+      for (int j = 1; j <= i; j++)
+     {
+         Console.Write(" * ");
+     }
+      Console.WriteLine("");
+  }
+}
+
+
+
+
+
+
+void Triangle(int height, int width)
+
+ {
+     for (int i = 0; i < height; i++) 
+     {
+        for (int j = 0; j < width; j++)
+         {
+            Console.Write(" * ");  
+         }
+        Console.WriteLine();  
+     }
+ }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Shapes
-geometry shapes project in C#
+## Shapes
+
+# Birinchi qism
+![alt text](image.png)
+
+# Ikkinchi qism ***Switch***
+![alt text](image-1.png)
+
+# Uchinchi qism ***method*** Edge
+![alt text](image-2.png)
+
+# Tortinchi qism ***method*** Triangle
+![alt text](image-3.png)
